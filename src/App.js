3448fb45669562356d2a8aafa82441a30449fc87
blakewood84 <blake.wood.bst@visible.edu>
2021-02-25 18:53:53 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Select from 'react-select';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const options1 = [
+  { value: '0', label: 'administrator' },
+  { value: '1', label: 'dispatcher' },
+  { value: '2', label: 'maintenance' }
+]
+
+const options2 = [
+  { value: '0', label: 'administrator', app: 'maintenance' }
+]
+
+const styles = { 
+  width: '250px'
+};
+
+const colourStyles = {
+  
+  select: (provided, state) => {
+    const border = state.isSelected ? '1px solid black' : '1px solid black';
+    const width = '500px';
+
+    return { ...provided, border, width}
+  },
+  menu: styles => ({ ...styles, width: '500px'}),
+  control: (provided, state) => {
+    const border = '1px solid black';
+    const width = '500px';
+
+    return { ...provided, border, width}
+  }
+}
+
+
+class App extends React.Component {
+  
+  constructor(props){
+    super(props);
+
+    this.state = {
+      roles: {},
+    }
+  }
+
+  onChangeHandler(application, event)
+  {
+    let roles  = {...this.state.roles };
+    event.length > 0 ? roles[application] = event.map(option => option.value) : delete roles[application];
+    this.setState({ roles: roles }, () => console.log(this.state.roles));
+  }
+
+  render() {
+    
+    return (
+      <div className="App">
+        <Select isMulti options={options1} styles={colourStyles} onChange={ this.onChangeHandler.bind(this, 'dispatch') } />
+        <br /><br />
+        <Select isMulti options={options2} styles={colourStyles} onChange={ this.onChangeHandler.bind(this, 'maintenance') } />
+      </div>
+    
+    )}
 }
 
 export default App;
